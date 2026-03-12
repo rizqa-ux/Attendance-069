6ec2e2e526a7fbd5faafed86678a9a797f07e234
mhsastratech11 <mhsastratech11@polytechnic.astra.ac.id>
2026-03-12 08:51:28 +0700

    Penambahan File AttendanceApp-069

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.topSection}>
+        <View style={styles.redBg} />
+        <View style={styles.blueBg} />
+        <View style={styles.greenBg} />
+        <View style={styles.orangeBg} />
+
+        <View style={styles.row}>
+          <View style={styles.red} />
+          <View style={styles.blue} />
+          <View style={styles.green} />
+          <View style={styles.orange} />
+        </View>
+      </View>
+      <View style={styles.bottomSection} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,73 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  topSection: {
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    height: 210,
+    paddingTop: 30,
+  },
+  redBg: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '12%',
+    height: 30,
+    backgroundColor: 'red',
+  },
+  blueBg: {
+    position: 'absolute',
+    top: 0,
+    left: '12%',
+    width: '38%',
+    height: 30,
+    backgroundColor: 'blue',
+  },
+  greenBg: {
+    position: 'absolute',
+    top: 0,
+    left: '50%',
+    width: '25%',
+    height: 30,
+    backgroundColor: 'green',
+  },
+  orangeBg: {
+    position: 'absolute',
+    top: 0,
+    left: '75%',
+    width: '25%',
+    height: 30,
+    backgroundColor: 'orange',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'flex-start', 
+    width: '100%',
+  },
+  red: {
+    width: '12%',
+    height: 50,
+    backgroundColor: 'red',
+    marginTop: -19, 
+  },
+  blue: {
+    width: '38%',
+    height: 50,
+    backgroundColor: 'blue',
+  },
+  green: {
+    width: '25%',
+    height: 100,
+    backgroundColor: 'green',
+  },
+  orange: {
+    width: '25%',
+    height: 150,
+    backgroundColor: 'orange',
+  },
+  bottomSection: {
+    flex: 1,
+    backgroundColor: '#3a3a3a',
   },
 });
